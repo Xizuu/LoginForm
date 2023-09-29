33c75c2c4660986c9b167f5abea4915f615fda26
RaihanNih <raihancoder1409@gmail.com>
2023-09-29 22:03:01 +0800

    Mengubah path dan memindahkan css

--- index.php
+++ index.php
@@ -5,12 +5,12 @@ require_once "vendor/autoload.php";
 use Steampixel\Route;
 
 const BASEPATH = '/';
-const ROOT = "src/";
-const RESOURCES = "resources/";
+const ROOT = "src";
+const RESOURCES = "resources";
 
-Route::add("/", fn () => include_once ROOT . "Views/Index.views.php");
-Route::add("/login", fn () => include_once ROOT . "Views/Login.views.php", ["post", "get"]);
-Route::add("/register", fn () => include_once ROOT . "Views/Register.views.php", ["post", "get"]);
-Route::add("/logout", fn () => include_once ROOT . "Logout.php");
+Route::add("/", fn () => include_once ROOT . "/Views/Index.views.php");
+Route::add("/login", fn () => include_once ROOT . "/Views/Login.views.php", ["post", "get"]);
+Route::add("/register", fn () => include_once ROOT . "/Views/Register.views.php", ["post", "get"]);
+Route::add("/logout", fn () => include_once ROOT . "/Logout.php");
 
 Route::run(BASEPATH);
--- src/Views/Register.views.php
+++ src/Views/Register.views.php
@@ -4,7 +4,7 @@ use RaihanNih\Factory\SessionFactory;
 use RaihanNih\Utils\Config;
 use RaihanNih\Utils\Mysql;
 
-$config = new Config(RESOURCES . "config.yml");
+$config = new Config(RESOURCES . "/config.yml");
 if (!SessionFactory::hasStarted("username")) {
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (isset($_POST["register"])) {
@@ -63,116 +63,7 @@ if (!SessionFactory::hasStarted("username")) {
     <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600|Raleway:400,700|Karla:400,700|Poppins:400,500,600,700|Montserrat:400|Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-    <style>
-        body {
-            background-color: #080825;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            min-height: 100vh;
-            font-family: 'Open Sans', sans-serif;
-        }
-
-        .form-container {
-            background-color: #ffffff;
-            border-radius: 10px;
-            box-shadow: 1px 5px 20px rgba(255, 255, 255, 0.2);
-            padding: 40px;
-            max-width: 400px;
-            max-height: 600px;
-            overflow-y: auto;
-            margin: 0 auto;
-        }
-
-        .form-container h5 {
-            text-align: center;
-            color: #000;
-            font-size: 32px;
-            font-weight: bold;
-            margin-bottom: 20px;
-        }
-
-        .login-form input[type="text"],
-        .login-form input[type="password"],
-        .login-form input[type="email"] {
-            border: 2px solid #ccc;
-            border-radius: 5px;
-            padding: 10px;
-            margin-bottom: 10px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-            outline: none;
-            transition: border-color 0.3s ease;
-            width: 100%;
-        }
-
-        .login-form input[type="text"]:focus,
-        .login-form input[type="password"]:focus,
-        .login-form input[type="email"]:focus {
-            border-color: #000;
-        }
-
-        .login-form input[type="text"]:hover,
-        .login-form input[type="password"]:hover,
-        .login-form input[type="email"]:hover {
-            border-color: #000;
-        }
-
-        .login-form .form-group {
-            margin-bottom: 20px;
-        }
-
-        .login-form label {
-            font-weight: bold;
-            margin-bottom: 10px;
-            display: block;
-        }
-
-        .login-form button {
-            background-color: #000;
-            color: #fff;
-            border: 2px solid #ccc;
-            border-radius: 5px;
-            padding: 15px 30px;
-            cursor: pointer;
-            transition: background-color 0.3s ease, border-color 0.3s ease, color 0.3s ease;
-            width: 100%;
-        }
-
-        .login-form button:hover {
-            background-color: #222;
-            border-color: #333;
-            color: #fff;
-        }
-
-        .register-divider {
-            text-align: center;
-            margin-top: 15px;
-        }
-
-        .register-divider hr {
-            border: none;
-            height: 2px;
-            background: #ddd;
-            margin: 10px 0;
-        }
-
-        .register-link {
-            text-align: center;
-            margin-top: 10px;
-        }
-
-        .register-link a {
-            text-decoration: none;
-            color: #000;
-            transition: border-bottom 0.3s ease;
-            margin-bottom: -2px;
-        }
-
-        .register-link a:hover,
-        .register-link a:focus {
-            border-bottom: 1px solid #000;
-        }
-    </style>
+    <link rel="stylesheet" href="<?= RESOURCES ?>/css/Register.css">
 </head>
 
 <body>
